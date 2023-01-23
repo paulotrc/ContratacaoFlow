662db4d0ebabe90714e134383d5a271e0c205eeb
paulotrc <paulotrc@gmail.com>
2023-01-23 17:33:56 -0300

    [NEW] First Commit from structure

---
+++ src/main/java/br/paulotrc/contratacaoflow/ContratacaoflowApplication.java
@@ -0,0 +1,13 @@
+package br.paulotrc.contratacaoflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContratacaoflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContratacaoflowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/paulotrc/contratacaoflow/ContratacaoflowApplicationTests.java
@@ -0,0 +1,13 @@
+package br.paulotrc.contratacaoflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContratacaoflowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
